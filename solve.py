5b23e52753c039af4aea2404b9048efd0cf7b6cd
Mohamed Haddi <m.haddi.95@gmail.com>
2020-07-25 10:01:23 +0100

    works with any file name now, shows messages while extracting and when exiting (exits either when more than one file is extracted, or when format isn't supported

--- solve.py
+++ solve.py
@@ -1,69 +1,132 @@
 #!/usr/bin/python
 
-# Important: sudo apt-get install ppmd kgb arj rzip bzip2 cabextract nomarch
-
 import os
-import subprocess 
+import subprocess
+from termcolor import colored # pip install termcolor
 
-def check(a):
+""" Important:
+
+This script only works with single files that have been compressed multiple
+times through different compression programs, each time compressed by itself
+without any other files.
+The script ends once multiple files were extracted.
+
+1. Create a new directory (e.g.: mkdir extract/) (child of the current one),
+and copy your archive file there with no other files. 
+
+Supported compressions:
+KGB, ARJ, PPMD, ZIP, RZIP, GZIP, BZIP2, TAR, CAB, ARC, XZ, 7z, ZOO, RAR
+
+2. Install these data compression programs:
+`sudo apt-get install ppmd kgb arj rzip bzip2 cabextract nomarch zoo`
+
+Note: You may need to install ppmd and zoo packages manually 
+
+3. Set the variable `path` to your new directory name. """
+
+path = "extract" # Your new directory name
+
+# Context manager for changing the current working directory
+class cd:
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
+# Return file type
+def file_type(a):
     r = ''
     for i in range(0, len(a)):
-        if a[i] == ',' or a[i] == '-' :
+        if a[i] == ',' or a[i] == '-':
             break 	
         else:
             r += a[i]
     return r
 
-i = 0
-while True:
-    c = os.popen("file secret").read()
-    s = check(c)
-    i += 1
-    current = str(i) + ": " + c + " => " + s + "\n"
-    print(current)
+# Enter then exit directory
+with cd(path):
 
-    if s == "secret: KGB Archiver file with compression level 3\n" :
-        os.system('mv secret secret.kgb && kgb secret.kgb && rm secret.kgb')
-    
-    elif s == "secret: ARJ archive data":
-        os.system('mv secret secret.arj && arj e secret.arj && rm secret.arj')
-    
-    elif s == "secret: PPMD archive data\n":
-        os.system('mv secret secret.ppmd && ppmd d secret.ppmd && rm secret.ppmd')
-    
-    elif s == "secret: rzip compressed data ":
-        os.system('mv secret secret.rz && rzip -d secret.rz')	
-    
-    elif s == "secret: gzip compressed data":
-        os.system('mv secret secret.gz && gzip -d secret.gz')
-    
-    elif s == "secret: POSIX tar archive (GNU)\n":
-        os.system('mv secret secret.tar && tar -xvf secret.tar && rm secret.tar')
-    
-    elif s == "secret: Zip archive data":
-        os.system('mv secret secret.zip && unzip secret.zip && rm secret.zip ')
-    
-    elif s == "secret: Microsoft Cabinet archive data":
-        os.system('mv secret secret.cab && cabextract -F "secret" secret.cab && rm secret.cab')	
-    
-    elif s == "secret: bzip2 compressed data":
-        os.system('mv secret secret.bz2 && bzip2 -d secret.bz2 ')
-    
-    elif s == "secret: ARC archive data":
-        os.system('mv secret secret.arc && nomarch secret.arc && rm secret.arc ')
-    
-    elif s == "secret: XZ compressed data\n":
-        os.system(' mv secret secret.xz && xz -d secret.xz  ')
-    
-    elif s == "secret: 7":
-        os.system('mv secret secret.7z && p7zip -d secret.7z')
-    
-    elif s == "secret: Zoo archive data":
-        os.system('mv secret secret.zoo && zoo -extract secret.zoo && rm secret.zoo')
-    
-    elif s == "secret: RAR archive data":
-        os.system('mv secret secret.rar && unrar e secret.rar && rm secret.rar')
+    i = 0
 
-    else:
-        print("Extraction ended: Either not an archive file or a non-handled format.\n'file secret' output: " + c + "\nIf this is a new format add it as a new elif statement.")
-        break
+    while True:
+
+        file_name = os.popen("find . -maxdepth 1 -not -type d").read()
+
+        # if more than one file, exit
+        if (file_name.count('\n') > 1):
+            print colored("=> end of decompression:\nmultiple files were extracted.", "magenta")
+            break
+        else:
+            file_name = file_name.rstrip('\n')
+
+        i += 1
+        print colored("\n" + str(i) + " => " + "Detecting type of file: " + file_name + "\n", "cyan")
+        c = os.popen("file " + file_name).read()
+        print("`file` command output:\n" + c)
+        s = file_type(c)
+    
+        if s == file_name + ": KGB Archiver file with compression level 3\n" :
+            print colored("Extracting from KGB file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.kgb && kgb ' + file_name + '.kgb && rm ' + file_name + '.kgb')
+        
+        elif s == file_name + ": ARJ archive data":
+            print colored("Extracting from ARJ file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.arj && arj e ' + file_name + '.arj && rm ' + file_name + '.arj')
+        
+        elif s == file_name + ": PPMD archive data\n":
+            print colored("Extracting from PPMD file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.ppmd && ppmd d ' + file_name + '.ppmd && rm ' + file_name + '.ppmd')
+        
+        elif s == file_name + ": rzip compressed data ":
+            print colored("Extracting from rzip file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.rz && rzip -d ' + file_name + '.rz')	
+        
+        elif s == file_name + ": gzip compressed data":
+            print colored("Extracting from gzip file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.gz && gzip -d ' + file_name + '.gz')
+        
+        elif s == file_name + ": POSIX tar archive (GNU)\n":
+            print colored("Extracting from POSIX file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.tar && tar -xvf ' + file_name + '.tar && rm ' + file_name + '.tar')
+        
+        elif s == file_name + ": Zip archive data":
+            print colored("Extracting from Zip file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.zip && unzip ' + file_name + '.zip && rm ' + file_name + '.zip ')
+        
+        elif s == file_name + ": Microsoft Cabinet archive data":
+            print colored("Extracting from CAB file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.cab && cabextract ' + file_name + '.cab && rm ' + file_name + '.cab')	
+        
+        elif s == file_name + ": bzip2 compressed data":
+            print colored("Extracting from bzip2 file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.bz2 && bzip2 -d ' + file_name + '.bz2 ')
+        
+        elif s == file_name + ": ARC archive data":
+            print colored("Extracting from ARC file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.arc && nomarch ' + file_name + '.arc && rm ' + file_name + '.arc ')
+        
+        elif s == file_name + ": XZ compressed data\n":
+            print colored("Extracting from XZ file...\n", "grey")
+            os.system(' mv ' + file_name + ' ' + file_name + '.xz && xz -d ' + file_name + '.xz  ')
+        
+        elif s == file_name + ": 7":
+            print colored("Extracting from 7z file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.7z && p7zip -d ' + file_name + '.7z')
+        
+        elif s == file_name + ": Zoo archive data":
+            print colored("Extracting from Zoo file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.zoo && zoo -e ' + file_name + '.zoo && rm ' + file_name + '.zoo')
+        
+        elif s == file_name + ": RAR archive data":
+            print colored("Extracting from RAR file...\n", "grey")
+            os.system('mv ' + file_name + ' ' + file_name + '.rar && unrar e ' + file_name + '.rar && rm ' + file_name + '.rar')
+    
+        else:
+            print colored("=> End of decompression:\nEither not an archive file or a non-handled format.\n\n=> Output of `file` command:\n" + c, "magenta")
+            print colored("If this format isn't handled by this script please add a new elif statement.", "red")
+            break
